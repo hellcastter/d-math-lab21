8d98dfc20553132b4c3a49eeef325b20fa21c23e
Victor Muryn <muryn.pn@ucu.edu.ua>
2023-02-09 15:53:32 +0200

    added gitignore and fixed pylint

--- task1/generate_graph.py
+++ task1/generate_graph.py
@@ -3,6 +3,7 @@ import networkx as nx
 import matplotlib.pyplot as plt
 from itertools import combinations, groupby
 
+
 # You can use this function to generate a random graph with 'num_of_nodes' nodes
 # and 'completeness' probability of an edge between any two nodes
 # If 'directed' is True, the graph will be directed
@@ -12,7 +13,7 @@ def gnp_random_connected_graph(num_of_nodes: int,
                                directed: bool = False,
                                draw: bool = False):
     """
-    Generates a random graph, similarly to an Erdős-Rényi 
+    Generates a random graph, similarly to an Erdős-Rényi
     graph, but enforcing that the resulting graph is conneted (in case of undirected graphs)
     """
 
@@ -22,8 +23,8 @@ def gnp_random_connected_graph(num_of_nodes: int,
         G = nx.Graph()
     edges = combinations(range(num_of_nodes), 2)
     G.add_nodes_from(range(num_of_nodes))
-    
-    for _, node_edges in groupby(edges, key = lambda x: x[0]):
+
+    for _, node_edges in groupby(edges, key=lambda x: x[0]):
         node_edges = list(node_edges)
         random_edge = random.choice(node_edges)
         if random.random() < 0.5:
@@ -32,26 +33,26 @@ def gnp_random_connected_graph(num_of_nodes: int,
         for e in node_edges:
             if random.random() < completeness:
                 G.add_edge(*e)
-                
-    for (u,v,w) in G.edges(data=True):
+
+    for (u, v, w) in G.edges(data=True):
         w['weight'] = random.randint(-5, 20)
-                
-    if draw: 
-        plt.figure(figsize=(10,6))
+
+    if draw:
+        plt.figure(figsize=(10, 6))
         if directed:
             # draw with edge weights
             pos = nx.arf_layout(G)
-            nx.draw(G,pos, node_color='lightblue', 
+            nx.draw(G, pos, node_color='lightblue',
                     with_labels=True,
-                    node_size=500, 
-                    arrowsize=20, 
+                    node_size=500,
+                    arrowsize=20,
                     arrows=True)
-            labels = nx.get_edge_attributes(G,'weight')
-            nx.draw_networkx_edge_labels(G, pos,edge_labels=labels)
-            
+            labels = nx.get_edge_attributes(G, 'weight')
+            nx.draw_networkx_edge_labels(G, pos, edge_labels=labels)
+
         else:
-            nx.draw(G, node_color='lightblue', 
-                with_labels=True, 
-                node_size=500)
-        
+            nx.draw(G, node_color='lightblue',
+                    with_labels=True,
+                    node_size=500)
+
     return G
